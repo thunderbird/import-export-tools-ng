38bea9e635c984ea6f8b0a09336757542d457108
John Bieling <john.bieling@gmx.de>
2022-06-27 21:18:11 +0200

    fix session restore

--- src/api/SessionRestore/implementation.js
+++ src/api/SessionRestore/implementation.js
@@ -15,6 +15,7 @@
 var { ExtensionCommon } = ChromeUtils.import("resource://gre/modules/ExtensionCommon.jsm");
 var { ExtensionSupport } = ChromeUtils.import("resource:///modules/ExtensionSupport.jsm");
 var { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
+var { SessionStoreManager } = ChromeUtils.import("resource:///modules/SessionStoreManager.jsm");
 
 var SessionRestore = class extends ExtensionCommon.ExtensionAPI {
   getAPI(context) {
@@ -25,9 +26,13 @@ var SessionRestore = class extends ExtensionCommon.ExtensionAPI {
     return {
       SessionRestore: {
 
+        isRestored() {
+          return !!SessionStoreManager._restored;
+        },
+
         onStartupSessionRestore: new ExtensionCommon.EventManager({
           context,
-          name: "SessionRestore.onNotifyBackground",
+          name: "SessionRestore.onStartupSessionRestore",
           register: (fire) => {
             let trackerId = self.observerTrackerNext++;
             self.observerTracker[trackerId] = fire.sync;

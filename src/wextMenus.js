91fee212b7e2a63984716560b96c08abfb647e2b
Christopher Leidigh <cleidigh@Gmail.com>
2023-05-30 18:15:54 -0400

    help tools menu localization

--- src/wextMenus.js
+++ src/wextMenus.js
@@ -740,7 +740,7 @@ async function createMenus(menuType, menuArray, options) {
 
 
 await new Promise(resolve => window.setTimeout(resolve, 100));
-//await createtitles("folderCtxMenuMSGS", folderCtxMenuSet, null);
+await createtitles("toolsCtxMenuMSGS", toolsCtxMenuSet, null);
 //await editMenus("", toolsCtxMenuSet, null);
 
 

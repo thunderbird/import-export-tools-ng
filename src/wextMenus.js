7f3f2d21a78a19ec50a497257ff4939ab984a370
Christopher Leidigh <cleidigh@Gmail.com>
2023-11-08 16:16:59 -0500

    cleanup wextMenus

--- src/wextMenus.js
+++ src/wextMenus.js
@@ -886,7 +886,7 @@ async function wextctx_toolsMenu(ctxEvent) {
 }
 
 async function wextctx_folderMenu(ctxEvent) {
-  console.log(ctxEvent);
+  //console.log(ctxEvent);
   var params = {};
   // we need the accountId and path of the folder to get 
   // the actual selected folder in legacy side
@@ -1100,7 +1100,6 @@ async function menusUpdate(info, tab) {
 
   // update for an account item
   if (accountId && !folderPath) {
-    console.log("account menu")
     await messenger.menus.update(folderCtxMenu_Exp_Account_Id, { visible: true });
 
     let newTitle = localizeMenuTitle("folderCtxMenu_Exp_Account_Id.title") + " - " + info.selectedAccount.name;
@@ -1131,10 +1130,8 @@ async function menusUpdate(info, tab) {
         (await messenger.accounts.get(accountId)).type == "nntp") {
         await messenger.menus.update(folderCtxMenu_Imp_MboxFiles_Id, { enabled: false });
         await messenger.menus.refresh();
-        console.log("account imp mbox disable")
 
       } else {
-        console.log("account store check")
 
         // we are a LF, check store type
         let mailStoreType = await getMailStoreFromFolderPath(accountId, folderPath);
@@ -1147,7 +1144,6 @@ async function menusUpdate(info, tab) {
           
 
         } else {
-        console.log("account maildir imp mbox disable")
           
           await messenger.menus.update(folderCtxMenu_Imp_MboxFiles_Id, { visible: false });
 
@@ -1167,7 +1163,6 @@ async function menusUpdate(info, tab) {
   }
 
   // default visibility
-  console.log("def visibility")
   await messenger.menus.update(folderCtxMenu_Exp_Account_Id, { visible: false });
   await messenger.menus.update(folderCtxMenu_Imp_MaildirFiles_Id, { visible: false });
   await messenger.menus.update(folderCtxMenu_Exp_FolderMbox_Id, { visible: true });
@@ -1194,7 +1189,6 @@ async function menusUpdate(info, tab) {
 
   // For folder ctx menu show or hide items based on store type, mbox or maildir
   if (info.menuIds[0] == folderCtxMenu_TopId) {
-    console.log("check store type")
 
     let mailStoreType = await getMailStoreFromFolderPath(accountId, folderPath);
     // 0 = mbox
@@ -1222,7 +1216,6 @@ async function menusUpdate(info, tab) {
       (await messenger.accounts.get(accountId)).type == "imap" ||
       (await messenger.accounts.get(accountId)).type == "rss" ||
       (await messenger.accounts.get(accountId)).type == "nntp") {
-      console.log("disable for imap")
       await messenger.menus.update(folderCtxMenu_Imp_MboxFiles_Id, { enabled: false });
     } else {
       await messenger.menus.update(folderCtxMenu_Imp_MboxFiles_Id, { enabled: true });
@@ -1246,7 +1239,6 @@ async function copyToClipboard(ctxEvent, tab) {
 
   if (ctxEvent.pageUrl == undefined && ctxEvent.parentMenuItemId == msgCtxMenu_CopyToClipboard_Id) {
     params.selectedMsgs = ctxEvent.selectedMessages.messages;
-    console.log("msg", params.selectedMsgs)
 
   } else {
     let msg = (await messenger.messageDisplay.getDisplayedMessage(tab.id));

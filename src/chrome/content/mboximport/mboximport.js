b389ebfb9f273922d0065e442298f7c8f6bef75e
Christopher Leidigh <cleidigh@Gmail.com>
2024-08-03 16:26:27 -0400

    remove From prologue

--- src/chrome/content/mboximport/mboximport.js
+++ src/chrome/content/mboximport/mboximport.js
@@ -1591,7 +1591,8 @@ function writeDataToFolder(data, msgFolder, file, removeFile) {
 	data = data.replace(/^From\s+.+\r?\n/, "");
 
 	// Prologue needed to add the message to the folder
-	var prologue = "From - " + nowString + "\n"; // The first line must begin with "From -", the following is not important
+	//var prologue = "From - " + nowString + "\n"; // The first line must begin with "From -", the following is not important
+	var prologue = "";
 	// If the message has no X-Mozilla-Status, we add them to it
 	if (!data.includes("X-Mozilla-Status"))
 		prologue = prologue + "X-Mozilla-Status: 0000\nX-Mozilla-Status2: 00000000\n";

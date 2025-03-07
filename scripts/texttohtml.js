e0e1727645b8aa30ef9922ae77bff56d1820efde
Christopher Leidigh <cleidigh@Gmail.com>
2025-03-06 19:22:24 -0500

    more regex

--- scripts/texttohtml.js
+++ scripts/texttohtml.js
@@ -2,22 +2,30 @@
 
 const fs = require('fs-extra');
 
-var source = fs.readFileSync("scripts/test2.html", { encoding: 'utf8' });
+var source = fs.readFileSync("scripts/t3.txt", { encoding: 'utf8' });
 let t = "7567\nyyy\n>yuj\n>ygfd\nheok"
-let regexp = /(>[\s\S]*)\r\n^[^>]/m;
+let regexp = /$[^>]([ \S\s]*^)[^>]/gm;
 
 //const array = [...source.matchAll(regexp)];
-//const array = source.match(regexp);
+const array = source.match(regexp);
+console.log(array)
 
-let output = source.replaceAll(/>\r\n$/gm, "<div style='padding-left: 40px'>&gt;</div>");
+let output = source.replaceAll(/^>\r\n/gm, "</pre><div style='padding-left: 40px'>&gt;</div>\r\n<pre>");
+output = output.replaceAll(/^>>\r\n/gm, "<div style='padding-left: 80px'>&gt;&gt;</div>\r\n");
+output = output.replaceAll(/^>>>\r\n/gm, "<div style='padding-left: 120px'>&gt;&gt;&gt;</div>\r\n");
+output = output.replaceAll(/^>>>>\r\n/gm, "<div style='padding-left: 160px'>&gt;&gt;&gt;&gt;</div>\r\n");
 
-output = output.replaceAll(/^>([^>][ \S]*)\r\n$/gm, "<div style='padding-left: 40px'>&gt;$1</div>");
+
+
+output = output.replaceAll(/^>([^>][ \S]*)\r\n/gm, "<div style='padding-left: 40px'>&gt;$1</div>");
 
 output = output.replaceAll(/^>>([^>][ \S]*)\r\n/gm, "<div style='padding-left: 80px'>&gt;&gt;$1</div>");
 output = output.replaceAll(/^>>>([^>][ \S]*)\r\n/gm, "<div style='padding-left: 120px'>&gt;&gt;&gt;$1</div>");
 output = output.replaceAll(/^>>>>([^>][ \S]*)\r\n/gm, "<div style='padding-left: 150px'>&gt;&gt;&gt;&gt;$1</div>");
+
+
 output = `<html><body><pre>${output}</pre></body></html>`;
 
-console.log(output)
+//console.log(output)
 
 fs.writeFileSync("scripts/out.html", output,{ encoding: 'utf8' });

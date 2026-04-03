a8c4e0f8afa063554a905028e774cf551a822b77
Christopher Leidigh <cleidigh@Gmail.com>
2026-04-03 16:43:07 -0400

    notification options in options, translation base

--- scripts/translate-gc2.js
+++ scripts/translate-gc2.js
@@ -20,7 +20,7 @@ const translate = new Translate({ projectId, key });
 */
 
 var translationArray = [
-	{ key: "Folder.label", text: "Folder"},
+	{ key: "setMsgExpDate.label", text: "Set exported message files date to message sent date"},
 
 
 ];

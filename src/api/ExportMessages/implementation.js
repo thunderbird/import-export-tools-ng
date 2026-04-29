d695a7e1aad134c60f95350719a0c303d57a43f7
Christopher Leidigh <cleidigh@Gmail.com>
2026-04-29 16:22:06 -0400

    cleanup unused functions

--- src/api/ExportMessages/implementation.js
+++ src/api/ExportMessages/implementation.js
@@ -22,17 +22,10 @@ var osPathSeparator = os.includes("win")
   ? "\\"
   : "/";
 
-
-// add Date now to query for debugging, thanks JB
-//dateNow = new Date();
-
+// we run this as an ES6 module for performance
 var { exportMessages } = ChromeUtils.importESModule(
   "resource://ietng/api/ExportMessages/Modules/exportMessages.mjs?" + ietngExtension.manifest.version + new Date()
 );
-
-//var { sorttable } = ChromeUtils.importESModule(
-//  "resource://ietng/api/commonModules/sorttable.js?" + ietngExtension.manifest.version + new Date()
-//);
 
 function getThunderbirdVersion() {
   let parts = Services.appinfo.version.split(".");
@@ -54,9 +47,6 @@ var ExportMessages = class extends ExtensionCommon.ExtensionAPI {
       ExportMessages: {
 
         async exportMessagesES6(expTask) {
-          //console.log(self.context)
-          //console.log(new Date());
-          //console.log(expTask)
 
           return exportMessages.exportMessagesES6(expTask, self.context, emitter);
         },
@@ -116,11 +106,8 @@ var ExportMessages = class extends ExtensionCommon.ExtensionAPI {
 
         openFileDialog: async function (mode, title, initialDir, filter) {
 
-          let winCtx = msgWindow;
-          const tbVersion = getThunderbirdVersion();
-          if (tbVersion.major >= 120) {
-            winCtx = msgWindow.browsingContext;
-          }
+          let winCtx = msgWindow.browsingContext;
+
           let fp = Cc["@mozilla.org/filepicker;1"].createInstance(Ci.nsIFilePicker);
           let resultObj = {};
           fp.init(winCtx, title, mode);
@@ -153,8 +140,6 @@ var ExportMessages = class extends ExtensionCommon.ExtensionAPI {
 
           if (mode === Ci.nsIFilePicker.modeGetFolder) {
             resultObj.folder = fp.file.path;
-            console.log(resultObj);
-
           }
           return resultObj;
         },
@@ -186,89 +171,6 @@ var ExportMessages = class extends ExtensionCommon.ExtensionAPI {
     if (isAppShutdown) {
       return;
     }
-  }
-
-  // private msg processing functions
-
-  async _readMsg(expTask, msgEntry) {
-
-    return this._getRawMessage(msgEntry.msgUri, expTask.getMsg.convertData);
-  }
-
-  async _saveMsgAttachments(expTask, msgEntry) {
-    //console.log(msgEntry)
-    //console.log(msgEntry.msgHdr)
-    //console.log(msgEntry.msgHdr.subject)
-
-
-    let containerName = `${msgEntry.msgHdr.mime2DecodedSubject}-Atts`;
-    containerName = containerName.replace(/[\/\\:<>*\?\"\|]/g, "_");
-
-    let uName = await IOUtils.createUniqueDirectory(expTask.exportContainer.directory, containerName);
-    var MailService = MailServices.messageServiceFromURI(msgEntry.msgUri);
-
-    //console.log(decodeURIComponent(attachments[0].url))
-    let msgUri = msgEntry.msgUri;
-    console.log(msgUri);
-    console.log(MailService.getUrlForUri(msgUri).spec);
-
-    let msgAttPartName = msgEntry.attachments[0].partName;
-    let msgAttName = msgEntry.attachments[0].name;
-
-    let msgAttUrl = `${decodeURIComponent(MailService.getUrlForUri(msgUri).spec)}?part=${msgAttPartName}&filename=${msgAttName}`;
-    console.log(msgAttUrl);
-    var attFile = Cc["@mozilla.org/file/local;1"].createInstance(Ci.nsIFile);
-    attFile.initWithPath(PathUtils.join(uName, msgAttName));
-
-    msgWindow.messenger.saveAttachmentToFile(attFile, msgAttUrl, msgUri, msgEntry.attachments[0].contentType, null);
-
-  }
-
-
-  async _getRawMessage(msgUri, aConvertData) {
-
-    let service = MailServices.messageServiceFromURI(msgUri);
-    return new Promise((resolve, reject) => {
-      let streamlistener = {
-        _data: [],
-        _stream: null,
-        onDataAvailable(aRequest, aInputStream, aOffset, aCount) {
-          if (!this._stream) {
-            this._stream = Cc[
-              "@mozilla.org/scriptableinputstream;1"
-            ].createInstance(Ci.nsIScriptableInputStream);
-            this._stream.init(aInputStream);
-          }
-          this._data.push(this._stream.read(aCount));
-        },
-        onStartRequest() { },
-        onStopRequest(request, status) {
-          if (Components.isSuccessCode(status)) {
-            resolve(this._data.join(""));
-          } else {
-            reject(
-              new ExtensionError(
-                `Error while streaming message <${msgUri}>: ${status}`
-              )
-            );
-          }
-        },
-        QueryInterface: ChromeUtils.generateQI([
-          "nsIStreamListener",
-          "nsIRequestObserver",
-        ]),
-      };
-
-      // This is not using aConvertData and therefore works for news:// messages.
-      service.streamMessage(
-        msgUri,
-        streamlistener,
-        null, // aMsgWindow
-        null, // aUrlListener
-        aConvertData, // aConvertData
-        "" //aAdditionalHeader
-      );
-    });
   }
 
   // private support functions

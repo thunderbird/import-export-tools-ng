aa84572025d70e22b01dbcf3d8305ecc5460ca9f
Christopher Leidigh <cleidigh@Gmail.com>
2026-08-27 15:15:47 -0400

    fix uninitialized namFormat => simple

--- src/Modules/prefMgmt.mjs
+++ src/Modules/prefMgmt.mjs
@@ -233,6 +233,9 @@ async function _migrateLegacyPrefs() {
   // we need to convert some legacy pref vals to updated vals
 
   // mode 0 not supported, move to 2 == dropdown mode and set default pattern
+  
+  let nameFormatLS = await prefCmds.getPref("export.names.defaults.msgNameFormatType");
+  console.log("nf ls", nameFormatLS)
   let nameFormat = await messenger.LegacyPrefs.getPref(`${addonRootPref}.exportEML.filename_format`);
 
   if (nameFormat != 1 && nameFormat != 3) {
@@ -251,12 +254,14 @@ async function _migrateLegacyPrefs() {
 
   // convert filename type to new format for migration
   nameFormat = await messenger.LegacyPrefs.getPref(`${addonRootPref}.exportEML.filename_format`);
-  if (nameFormat == 2) {
+  if (!nameFormat || nameFormat == 2) {
     await prefCmds.setPref(`export.names.defaults.msgNameFormatType`, "simple");
   } else {
     await prefCmds.setPref(`export.names.defaults.msgNameFormatType`, "extended");
   }
 
+  nameFormatLS = await prefCmds.getPref("export.names.defaults.msgNameFormatType");
+  console.log("final nf ls", nameFormatLS)
 
   // next set all userPrefs from legacy map
   // were also cleaning up names and structure
@@ -289,7 +294,7 @@ async function _migrateLegacyPrefs() {
       //console.warn("IETNG: Removing deadpref:", legacyKey);
     }
     // clear the legacy pref regardless
-    //messenger.LegacyPrefs.clearUserPref(`${addonRootPref}.${legacyKey}`);
+    messenger.LegacyPrefs.clearUserPref(`${addonRootPref}.${legacyKey}`);
   }
 }
 
